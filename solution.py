dd31a4005ee14c9691ef5095cdda245d1ef182a4
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-12 09:59:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
+chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x: np.array, y: np.array) -> bool:
+    alpha=0.08
+    stat, p_val = mannwhitneyu(x, y, alternative='two-sided')
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    if p_val < alpha:
+        return True
+    else:
+        return False
