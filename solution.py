0599d1a5d3ba863e949d759ac7ee3a4e7760a5b3
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-12 22:40:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,24 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-     n_permutations=10000
-     alpha=0.08
-     observed_difference = np.mean(y) - np.mean(x)
-
-     combined = np.concatenate([x, y])
-
-     differences = np.zeros(n_permutations)
-
-     for i in range(n_permutations):
-         permuted = np.random.permutation(combined)
-         x_permuted = permuted[:len(x)]
-         y_permuted = permuted[len(x):]
-
-     differences[i] = np.mean(y_permuted) - np.mean(x_permuted)
-
-     p_value = (differences >= observed_difference).sum() / n_permutations
-
-     return p_value < alpha
+    alpha=0.08
+    t_stat, p_value = ttest_ind(x, y, equal_var=False)
+    return p_value/2 < alpha # двухсторонний тест, делаем односторонним
